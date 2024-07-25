1f65ab98b3819d0a9e6f7afa81c059ae144c22f5
Sohan <gundojusohan@gmail.com>
2024-07-25 22:34:10 +0530

    Updates

---
+++ precache-manifest.e7820c81ed90def1fd8aeb6f40367b62.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c391abf3bc8a97f0be6aeaa6415c017",
+    "url": "/AuthGen-Frontend/index.html"
+  },
+  {
+    "revision": "4e5b25049217d7eff826",
+    "url": "/AuthGen-Frontend/static/js/10.6da65b1d.chunk.js"
+  },
+  {
+    "revision": "7c608176aed30895b301",
+    "url": "/AuthGen-Frontend/static/js/2.1e659b97.chunk.js"
+  },
+  {
+    "revision": "f0d55d9e91500fc69ebc90b43f8df3d2",
+    "url": "/AuthGen-Frontend/static/js/2.1e659b97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49a8e1076acab925eb8d",
+    "url": "/AuthGen-Frontend/static/js/3.3737fb4c.chunk.js"
+  },
+  {
+    "revision": "ee46db9d7d485d6508b5",
+    "url": "/AuthGen-Frontend/static/js/4.b576cd97.chunk.js"
+  },
+  {
+    "revision": "2ac0ce0950549cbfa14d",
+    "url": "/AuthGen-Frontend/static/js/5.9d92d8ac.chunk.js"
+  },
+  {
+    "revision": "93b1c1ad411d397bad16",
+    "url": "/AuthGen-Frontend/static/js/6.d9c7b2a9.chunk.js"
+  },
+  {
+    "revision": "50f81c42c468d04dff28",
+    "url": "/AuthGen-Frontend/static/js/7.a383b8f4.chunk.js"
+  },
+  {
+    "revision": "f0d9119a0ee0d2a8e63e",
+    "url": "/AuthGen-Frontend/static/js/8.86d759d8.chunk.js"
+  },
+  {
+    "revision": "0317feed9666a0c37cae",
+    "url": "/AuthGen-Frontend/static/js/9.4f6d3a09.chunk.js"
+  },
+  {
+    "revision": "e0e834324b33d86b9c96",
+    "url": "/AuthGen-Frontend/static/js/main.d3a65a71.chunk.js"
+  },
+  {
+    "revision": "68d17e990b8c6a69d6d5",
+    "url": "/AuthGen-Frontend/static/js/runtime-main.ca100460.js"
+  }
+]);
